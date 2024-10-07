fa99e962bc6ae1ef5039fa5f5f51cc1d51227575
DanielXero <117545957+DanielXero@users.noreply.github.com>
2024-10-07 18:34:53 -0300

    Add files via upload

--- Practico sql/serie2.sql
+++ Practico sql/serie2.sql
@@ -28,13 +28,15 @@ ALTER TABLE gasto
 /*
 Ejercicio Nº 1
 Escribir una consulta que muestre todos los datos de todos los conserjes.
-Registros devueltos: 220*/
+Registros devueltos: 220
+*/
 SELECT * FROM conserje
 
 /*
 Ejercicio Nº 2
 Mostrar solamente el nro. de conserje (idconserje) y el nombre (apeynom).
-Registros devueltos: 220*/
+Registros devueltos: 220
+*/
 
 select idconserje, apeynom from conserje
 
@@ -57,6 +59,42 @@ formatos diferentes, con las siguientes cabeceras “Sin formato”, “Redondeado a 1
 decimal”, “Truncado a 1 digito”. Usar función ROUND.
 */
 
+select * from gasto
 
+SELECT
+    idgasto,
+    importe * 1.2 AS gasto_original,
+    ROUND(importe * 1.2, 1) AS gasto_redondeado,
+    FLOOR(importe * 1.2 * 10) / 10.0 AS gasto_truncado
+FROM
+    gasto;
 
+/*
+Ejercicio Nº 5
+Listar el nombre (descripcion) y la población (poblacion) de cada Provincia.
+Salida Esperada:
+Registros devueltos: 24
+*/
+select descripcion, poblacion from provincia
 
+/*
+Ejercicio Nº 6
+Listar sin repetir, todos los códigos de provincia de la tabla “consorcio”. Usar Clausula DISTINCT
+*/
+SELECT * FROM consorcio
+SELECT DISTINCT idprovincia FROM consorcio
+
+/*
+Ejercicio Nº 7
+Listar los 15 primeros conserjes de la respectiva tabla. Usar cláusula TOP
+*/
+SELECT TOP 15 idconserje FROM consorcio ORDER BY idconserje ASC
+
+/*
+
+Ejercicio Nº 8
+Crear una consulta que muestre el Nombre y la dirección de los consorcios de la provincia de
+“Buenos Aires”. Tabla a utilizar: consorcio y idprovincia = 2
+*/
+
+SELECT nombre, direccion FROM consorcio WHERE idprovincia = 2 
